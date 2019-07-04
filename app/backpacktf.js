7950c494d7c89b60697b23eef75bf18473bcc392
Sergun <sergun6996@gmail.com>
2019-07-05 01:51:50 +0300

    Update backpacktf.js

--- app/backpacktf.js
+++ app/backpacktf.js
@@ -1,4 +1,5 @@
 const request = require("request");
+const util = require('./util.js');
 
 function Backpack(access_token, key) {
     this.baseUrl = 'https://backpack.tf/api/';
@@ -12,15 +13,29 @@ Backpack.prototype.getUserInfo = function(steamids, callback) {
     });
 };
 
+Backpack.prototype.getCurrencies = function(callback) {
+    request.get({url: this.baseUrl + 'IGetCurrencies/v1', qs: {'key': this.key}}, function(err, response, body) {
+        if (err) return callback(err);
+        body = JSON.parse(body).response;
+        if (body.success === 0) return callback(err = body.message);
+        return callback(err, body.currencies);
+    });
+};
+
 Backpack.prototype.isBanned = function(steamid, callback) {
     this.getUserInfo(steamid, function (err, response, body) {
+        let returnRes = false;
         if (err) return callback(err);
-        console.log(JSON.stringify(body));
-        if (body.users[steamid].bans.hasOwnProperty('all')) {
-            return callback(err, true); //  the user is banned
-        } else {
-            return callback(err, false);
+        if (util.getSafe(() => body.users[steamid].bans)) {
+            if (util.getSafe(() => body.users[steamid].bans.steamrep_scammer) === 1) {
+                returnRes = true;
+            } else if (util.getSafe(() => body.users[steamid].bans.all.end) === -1) {
+                returnRes = true; //  the user is banned
+            } else {
+                returnRes = false;
+            }
         }
+        return callback(err, returnRes);
     });
 };
 
