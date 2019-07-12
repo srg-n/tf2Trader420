ca20e62d2d360e9f71ea66afc158d402634ccbb8
Sergun <sergun6996@gmail.com>
2019-07-12 21:51:58 +0300

    promisify

--- app/backpacktf.js
+++ app/backpacktf.js
@@ -1,4 +1,4 @@
-const request = require("request");
+const requestPromise = require("request-promise");
 const util = require('./util.js');
 
 function Backpack(access_token, key) {
@@ -7,35 +7,41 @@ function Backpack(access_token, key) {
     this.key = key;
 }
 
-Backpack.prototype.getUserInfo = function(steamids, callback) {
-    request.get({url: this.baseUrl + 'users/info/v1', qs: {'key': this.key, 'steamids': steamids}}, function(err, response, body) {
-        return callback(err, response, JSON.parse(body));
+Backpack.prototype.getUserInfo = function (steamids) {
+    return requestPromise({
+        uri: this.baseUrl + 'users/info/v1',
+        qs: {'key': this.key, 'steamids': steamids},
+        json: true
+    })
+};
+
+Backpack.prototype.getCurrencies = function () {
+    return new Promise((resolve, reject) => {
+        requestPromise({url: this.baseUrl + 'IGetCurrencies/v1', qs: {'key': this.key}, json: true})
+            .then(function (body) {
+                if (body.response.success === 0) reject(body.response.message);
+                else resolve(body.response.currencies);
+            }).catch(function (err) {
+                reject(err);  //  api request error
+        })
     });
 };
 
-Backpack.prototype.getCurrencies = function(callback) {
-    request.get({url: this.baseUrl + 'IGetCurrencies/v1', qs: {'key': this.key}}, function(err, response, body) {
-        if (err) return callback(err);
-        body = JSON.parse(body).response;
-        if (body.success === 0) return callback(err = body.message);
-        return callback(err, body.currencies);
-    });
-};
-
-Backpack.prototype.isBanned = function(steamid, callback) {
-    this.getUserInfo(steamid, function (err, response, body) {
-        let returnRes = false;
-        if (err) return callback(err);
-        if (util.getSafe(() => body.users[steamid].bans)) {
-            if (util.getSafe(() => body.users[steamid].bans.steamrep_scammer) === 1) {
-                returnRes = true;
-            } else if (util.getSafe(() => body.users[steamid].bans.all.end) === -1) {
-                returnRes = true; //  the user is banned
-            } else {
-                returnRes = false;
-            }
-        }
-        return callback(err, returnRes);
+Backpack.prototype.isBanned = function (steamid) {
+    return new Promise((resolve, reject) => {
+        this.getUserInfo(steamid)
+            .then(function (res) {
+                if (util.getSafe(() => res.users[steamid].bans)) {
+                    if (util.getSafe(() => res.users[steamid].bans.steamrep_scammer) === 1) {
+                        resolve(true);  //  the user is banned
+                    } else if (util.getSafe(() => res.users[steamid].bans.all.end) === -1) {
+                        resolve(true);  //  the user is banned
+                    }
+                }
+                resolve(false); //  the user is not banned
+            }).catch(function (err) {
+            reject(err);  //  api request error
+        })
     });
 };
 
